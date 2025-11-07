f495e4362cf87ccd5b9cd3b39517616ed1ab5dc9
SPI ANT <antspiros06@gmail.com>
2025-11-07 17:48:57 +0200

    Implement Guessing Game in C
    This program implements a simple guessing game where the user has to guess a randomly generated number between 1 and 100. It provides feedback on whether the guess is too low or too high and counts the number of attempts.

---
+++ C/basics/Guess_Number_Game.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    srand(time(NULL));
+
+    int guess = 0;
+    int tries = 0;
+    int min = 1;
+    int max = 100;
+    int answer = (rand() % (max-min +1)) + min;
+
+    printf("Welcome to the Guessing Game!\n");
+
+    do{ 
+        printf("Guess a number between %d and %d: ", min, max);
+        scanf("%d", &guess);
+        tries++;
+        if(guess < answer){
+            printf("Too low! Try again.\n");
+        } else if(guess > answer){
+            printf("Too high! Try again.\n");
+        }
+        else{
+            printf("Congratulations! You guessed the correct number.\n");
+        }
+
+    }while (guess != answer);
+
+    printf("The answer is %d\n:", answer);
+    printf("You guessed it in %d tries\n", tries);
+
+    return 0;
+
+
+}
